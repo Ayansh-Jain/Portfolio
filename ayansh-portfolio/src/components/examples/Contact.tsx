5726ca884d9c6842cc8669e1f85d61ec698ddb12
Ayansh_Jain <ayanshjain2006@gmail.com>
2025-12-11 05:38:10 +0530

    third

--- ayansh-portfolio/src/components/examples/Contact.tsx
+++ ayansh-portfolio/src/components/examples/Contact.tsx
@@ -1,11 +1,5 @@
 import Contact from "../Contact";
-import { Toaster } from "@/components/ui/toaster";
 
 export default function ContactExample() {
-  return (
-    <>
-      <Contact />
-      <Toaster />
-    </>
-  );
+  return <Contact />;
 }
